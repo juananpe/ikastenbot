141696f5e4c4830258f089737ff0c770346a2810
Mikel Alejo <mikelalejobr@outlook.com>
2019-01-18 10:57:52 +0100

    Refactor: make structure.sql MySQL compliant
    MySQL doesn't seem to support `IF NOT EXISTS` when modifying a table's
    column, while MariaDB —the database used in the development environment—
    does. Instead of creating two versions of the SQL file —one for MySQL
    and another one for MariaDB— I have decided to go simple and make the
    file MySQL compliant for the sake of mantainability.

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,5 +1,5 @@
 /* Add language column to Longman's user table */
-ALTER TABLE `user` ADD IF NOT EXISTS `language` char(10) DEFAULT 'es'; 
+ALTER TABLE `user` ADD `language` char(10) DEFAULT 'es'; 
 
 DROP TABLE IF EXISTS `TFG`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -128,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `ganttproject`
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /* Add a ganttproject column plus a foreign key constraint */
-ALTER TABLE `user` ADD IF NOT EXISTS `ganttproject_id` INT NULL;
+ALTER TABLE `user` ADD `ganttproject_id` INT NULL;
 ALTER TABLE `user` ADD CONSTRAINT `fk_user_ganttproject_id` FOREIGN KEY (`ganttproject_id`) REFERENCES `ganttproject` (`id`) ON DELETE SET NULL;
 
 CREATE TABLE IF NOT EXISTS `task`
