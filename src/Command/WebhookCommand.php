20c166254b8c1079fe12602c99811032203ec8ce
Mikel Alejo <mikelalejobr@outlook.com>
2019-02-20 16:58:21 +0100

    Refactor: create command for setting and unsetting the webhook

---
+++ src/Command/WebhookCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Telegram;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WebhookCommand extends Command
+{
+    /**
+     * Command's name.
+     *
+     * @var string
+     */
+    protected static $defaultName = 'app:webhook';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Sets or unsets the Telegram webhook')
+            ->setHelp('Sets or unsets the Telegram webhook using the environment variables\'s content')
+            ->addOption(
+                'set',
+                null,
+                InputOption::VALUE_NONE,
+                'Set the webhook'
+            )
+            ->addOption(
+                'unset',
+                null,
+                InputOption::VALUE_NONE,
+                'Unset the webhook'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Setup Telegram object
+        $telegram = new Telegram(getenv('TELEGRAM_BOT_API_KEY'), getenv('TELEGRAM_BOT_USERNAME'));
+
+        // Get the options
+        $set = $input->getOption('set');
+        $unset = $input->getOption('unset');
+
+        // Check that inputs are meaningful
+        if ($set && $unset) {
+            $output->writeln('Can\'t do both!');
+
+            return;
+        }
+
+        if (!($set || $unset)) {
+            $output->writeln('Nothing to do.');
+
+            return;
+        }
+
+        // Set or unset the webhook
+        try {
+            $response = '';
+            if ($set) {
+                $response = $telegram->setWebhook(getenv('TELEGRAM_BOT_HOOK_URL'));
+            }
+
+            if ($unset) {
+                $response = $telegram->deleteWebhook();
+            }
+
+            if ($response->isOk()) {
+                $output->writeln($response->getDescription());
+            }
+        } catch (TelegramException $e) {
+            $output->writeln($e->getMessage());
+        }
+    }
+}
---
+++ tests/Command/WebhookCommandTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @covers \App\Command\WebhookCommand
+ *
+ * @internal
+ */
+final class WebhookCommandTest extends KernelTestCase
+{
+    /**
+     * The command to be executed.
+     *
+     * @var Symfony\Component\Console\Command\Command
+     */
+    private $command;
+
+    /**
+     * Command tester.
+     *
+     * @var CommandTester
+     */
+    private $commandTester;
+
+    /**
+     * Array containing the output messages of the command.
+     *
+     * @var array
+     */
+    private $messages;
+
+    public function setUp()
+    {
+        $this->messages = [
+            'both' => 'Can\'t do both!',
+            'empty' => 'Nothing to do.',
+        ];
+
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+        $this->command = $application->find('app:webhook');
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    /**
+     * @covers \App\Command\WebhookCommand::execute()
+     */
+    public function testNoParams()
+    {
+        $this->commandTester->execute(['command' => $this->command->getName()]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertContains(
+            $this->messages['empty'],
+            $output
+        );
+    }
+
+    /**
+     * @covers \App\Command\WebhookCommand::execute()
+     */
+    public function testBoth()
+    {
+        $this->commandTester->execute(
+            [
+                'command' => $this->command->getName(),
+                '--set' => true,
+                '--unset' => true,
+            ]
+        );
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertContains(
+            $this->messages['both'],
+            $output
+        );
+    }
+}
